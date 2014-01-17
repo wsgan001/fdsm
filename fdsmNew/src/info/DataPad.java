a7b026b0701cabb690470c267e3a72436dd71bed
blatoo <connygy@googlemail.com>
2014-01-17 14:14:09 +0100

    bbb

--- fdsmNew/src/info/DataPad.java
+++ fdsmNew/src/info/DataPad.java
@@ -5,5 +5,7 @@ public class DataPad {
 	public static String Pfad_doc = "D:/Ying/netflix/";
 	
 	public static String data3user = Pfad_doc+"data3user.txt";
+	
+	public static String mytext = "mytext";
 
 }
