77e7e200fa848201e73e35e52c5126161393608d
blatoo <connygy@googlemail.com>
2014-01-20 18:32:47 +0100

    bbb

--- fdsmNew/src/info/dataReadWrite.java
+++ fdsmNew/src/info/dataReadWrite.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class dataReadWrite {
 	
@@ -27,7 +29,13 @@ public class dataReadWrite {
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			
+			
+			
+			br.close();
+			
 		}catch(IOException e){
+			
+			e.printStackTrace();
 			
 		}
 		
@@ -37,12 +45,29 @@ public class dataReadWrite {
 		
 	}
 	
+	public static void test(){
+		
+		HashSet<String> hs = new HashSet<String>();
+		
+		hs.add("123");
+		hs.add("123");
+		hs.add("443");
+		
+		Iterator<String> it = hs.iterator();
+		
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+		
+	}
+	
 	
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		test();
 
 	}
 
