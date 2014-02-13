366a15072eedeeac0cb7bc15bb1398e6d7d29e1b
blatoo <connygy@googlemail.com>
2014-02-13 11:18:47 +0100

    gogo

---
+++ fdsmNew/src/info/DataSource.java
@@ -0,0 +1,33 @@
+package info;
+
+public class DataSource {
+	
+	public static String Pfad_doc = "D:/Ying/netflix/";
+	
+	public static String outputPath = "D:/Ying/netflix/output/";
+	
+	public static String data3user = Pfad_doc+"data3user.txt";
+	
+	public static int primaryColumn = 1;
+	
+	public static int secondaryColumn = 2;
+	
+	public static int ratingColumn = 3;
+	
+	public static int ratingCriterion = 3;
+	
+	public static String mytext = "mytext";
+	
+	public static String mytest = "mytest";
+	
+	public static String lineSeparate = System.lineSeparator();
+	
+	public static int selectModel = 1; // 1: select from ... to .., 2:random 
+	
+	public static int fromInd = 0;
+	
+	public static int toInd = 20000;
+	
+	public static int numberOfSamples = 20000;
+
+}
--- fdsmNew/src/info/dataReadWrite.java
+++ fdsmNew/src/info/dataReadWrite.java
@@ -1,41 +1,33 @@
 package info;
 
-import gnu.trove.set.hash.TIntHashSet;
+import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.map.hash.TIntIntHashMap;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
-import javolution.lang.Immutable;
 import javolution.util.FastBitSet;
-import javolution.util.FastSet;
-import javolution.util.FastSortedSet;
-import javolution.util.FastTable;
 import javolution.util.Index;
-//import gnu.trove.*;
-//
-//import org.apache.lucene.*;
-//import org.apache.lucene.util.OpenBitSet;
 
 public class dataReadWrite {
 
-	static String outputPath = DataPad.outputPath;
-	static String primaryIndexFile = outputPath + "PrimaryIndex.txt";
-	static String secondaryIndexFile = outputPath + "SecondaryIndex.txt";
+	static String outputPath = DataSource.outputPath;
+	static String primaryIndexTXT = outputPath + "PrimaryIndex.txt";
+	static String secondaryIndexTXT = outputPath + "SecondaryIndex.txt";
 	static String toker = ",";
-
+	static String selectedEntriesSceondaryIdTXT = DataSource.outputPath
+			+ "selectedEntriesSecondaryId.txt";
+	static String infoTXT = outputPath + "info.txt";
 
 	/**
 	 * generate the computer indexes for the data, the outputs are two computer
@@ -49,8 +41,8 @@ public class dataReadWrite {
 	 */
 	public static void dataIndex(String inputData, int primaryColumn,
 			int secondaryColumn) {
-		
-		File outputPath = new File(info.DataPad.outputPath);
+
+		File outputPath = new File(info.DataSource.outputPath);
 
 		File file = new File(inputData);
 
@@ -60,8 +52,8 @@ public class dataReadWrite {
 			System.exit(-1);
 
 		}
-		
-		if (!outputPath.exists()){
+
+		if (!outputPath.exists()) {
 			outputPath.mkdir();
 		}
 
@@ -95,7 +87,7 @@ public class dataReadWrite {
 			e.printStackTrace();
 
 		}
-		
+
 		Index[] indPrimary = fbsPrimaryList.toArray(new Index[0]);
 		Index[] indSecondary = fbsSecondaryList.toArray(new Index[0]);
 
@@ -103,30 +95,39 @@ public class dataReadWrite {
 
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(
-					primaryIndexFile));
-			
-			for(Index ind : indPrimary){
-				
-				bw.write(cnt + toker + ind);
-				
+					primaryIndexTXT));
+
+			for (Index ind : indPrimary) {
+
+				bw.write(cnt + "," + ind + DataSource.lineSeparate);
+
 				cnt++;
 			}
-			
 
 			bw.close();
 
 			cnt = 0;
-			
-			bw = new BufferedWriter(new FileWriter(secondaryIndexFile));
-			
-			for(Index ind : indSecondary){
-				
-				bw.newLine();
-				bw.write(cnt + toker + ind + DataPad.lineSeparate); 
-				
+
+			bw = new BufferedWriter(new FileWriter(secondaryIndexTXT));
+
+			for (Index ind : indSecondary) {
+
+				bw.write(cnt + "," + ind + DataSource.lineSeparate);
+
 				cnt++;
-				
+
 			}
+
+			bw.close();
+
+			bw = new BufferedWriter(new FileWriter(infoTXT));
+
+			bw.write("#Number of PrimaryIds:" + System.lineSeparator());
+			bw.write("numberOfPrimaryIds=" + indPrimary.length
+					+ System.lineSeparator());
+			bw.write("#Number Of SecondaryIds:" + System.lineSeparator());
+			bw.write("numberOfSecondaryIds=" + indSecondary.length
+					+ System.lineSeparator());
 
 			bw.close();
 		} catch (IOException e) {
@@ -136,27 +137,241 @@ public class dataReadWrite {
 
 	}
 
-	public static void hstest() {
+	/**
+	 * output the data which satisfy the criterion. The output file names
+	 * "selectedEntriesSecondaryId.txt".
+	 * 
+	 * selectedEntiresSecondaryId.txt Formatx: Computer_secondaryId:sum of the
+	 * Computer_primaryIds:Computer_PrimaryId,...,
+	 */
+	public static void selectEntries() {
+
+		File file = new File(DataSource.data3user);
+
+		TIntIntHashMap primaryIdList_HM = readIndexToHM(primaryIndexTXT, 2, 1,
+				",");
+
+		TIntIntHashMap secondaryIdList_HM = readIndexToHM(secondaryIndexTXT, 2,
+				1, ",");
+
+		TIntArrayList[] selectedEntries = new TIntArrayList[secondaryIdList_HM
+				.size()];
+
+		for (int i = 0; i < selectedEntries.length; i++) {
+
+			selectedEntries[i] = new TIntArrayList();
+
+		}
+
+		System.out.println("selectedEntires[] fertig and begin read...");
 
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter(outputPath+"hallo.txt"));
-			bw.write("aaa");
-			
-			bw.close();
-			
-			
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			String line = br.readLine();
+
+			int cnt = 0;
+
+			while (line != null) {
+
+				String[] lineInfo = line.split(toker);
+
+				int rating = Integer
+						.parseInt(lineInfo[DataSource.ratingColumn - 1]);
+
+				if (rating <= DataSource.ratingCriterion) {
+
+					line = br.readLine();
+
+					continue;
+
+				}
+
+				if (cnt % 100000 == 0) {
+					System.out.println("reading line" + cnt + ": " + line);
+				}
+
+				int secondaryId = secondaryIdList_HM.get(Integer
+						.parseInt(lineInfo[DataSource.secondaryColumn - 1]));
+
+				int primaryId = primaryIdList_HM.get(Integer
+						.parseInt(lineInfo[DataSource.primaryColumn - 1]));
+
+				selectedEntries[secondaryId].add(primaryId);
+
+				line = br.readLine();
+
+				cnt++;
+			}
+
+			br.close();
 		} catch (IOException e) {
-			// TODO: handle exception
+			e.printStackTrace();
+			System.exit(-1);
 		}
-		
-		
+
+		try {
+
+			System.out.println("begin to write...");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(
+					DataSource.outputPath + "selectedEntriesSecondaryId.txt"));
+
+			for (int i = 0; i < selectedEntries.length; i++) {
+
+				if (i % 10000 == 0) {
+					System.out.println(i);
+				}
+
+				selectedEntries[i].sort();
+
+				bw.write(i + ":" + selectedEntries[i].size() + ":");
+
+				for (int j = 0; j < selectedEntries[i].size(); j++) {
+
+					bw.write(selectedEntries[i].get(j) + ",");
+
+				}
+
+				bw.newLine();
+
+			}
+
+			bw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+	}
+
+	/**
+	 * read the index files into a HashMap, key is realId, value is the
+	 * computerId
+	 * 
+	 * @param indexTXT
+	 *            PrimaryIndex.txt and SecondaryIndex.txt
+	 * @param keyColumn
+	 *            default is 2
+	 * @param valueComlumn
+	 *            default is 1
+	 * @param toker
+	 *            default ","
+	 * @return
+	 */
+	public static TIntIntHashMap readIndexToHM(String indexTXT, int keyColumn,
+			int valueComlumn, String toker) {
+		TIntIntHashMap hm = new TIntIntHashMap();
+
+		File file = new File(indexTXT);
+
+		if (!file.exists()) {
+			System.err.println("File " + indexTXT + "doesn't existiert! ");
+			System.exit(-1);
+		}
+
+		try {
+
+			BufferedReader br = new BufferedReader(new FileReader(new File(
+					indexTXT)));
+
+			String line = br.readLine();
+
+			while (line != null) {
+
+				String[] entrys = line.split(toker);
+
+				hm.put(Integer.parseInt(entrys[keyColumn - 1]),
+						Integer.parseInt(entrys[valueComlumn - 1]));
+
+				line = br.readLine();
+
+			}
+
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return hm;
+	}
+
+	/**
+	 * Choose the Samples with select Model = 1
+	 * 
+	 * @param from
+	 *            ComputerSecondaryId+1
+	 * @param to
+	 *            ComputerSecondaryId+1
+	 */
+	public static void selectedEntries_Model_2(int from, int to) {
+
+		File file = new File(selectedEntriesSceondaryIdTXT);
+
+	}
+
+	public static HashMap<String, String> readInfoTXT() {
+
+		File file = new File(infoTXT);
+
+		if (!file.exists()) {
+			System.err.println(infoTXT + " doesn't exist");
+			System.exit(-1);
+		}
+
+		HashMap<String, String> hm = new HashMap<String, String>();
+
+		try {
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			String line = br.readLine();
+
+			while (line != null) {
+
+				if (!line.startsWith("#") && !line.trim().isEmpty()) {
+//					System.out.println("a");
+//					System.out.println("line = " + line);
+
+					String[] lineInfo = line.split("=");
+
+					hm.put(lineInfo[0].trim(), lineInfo[1].trim());
+
+//					System.out.println(line);
+//					System.out.println(lineInfo[0] + "," + lineInfo[1]);
+
+				}
+
+				// System.out.println(line);
+
+				line = br.readLine();
+
+			}
+
+			br.close();
+		} catch (IOException e) {
+			System.err.println(infoTXT + " doesn't exist!");
+
+			System.exit(-1);
+
+		}
+
+		return hm;
+	}
+
+	public static void test() {
+
+		HashMap<String, String> hm = readInfoTXT();
+
+		System.out.println(hm.size());
 
 	}
 
 	public static void main(String[] args) {
-		dataIndex(DataPad.data3user, 1, 2);
-		
-		
+
+		test();
+
 	}
 
 }
