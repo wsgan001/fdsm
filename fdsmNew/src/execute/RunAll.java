efdb1407f57a9c7a434bb331525f76f8dd7f59b6
blatoo <connygy@googlemail.com>
2014-06-13 14:05:57 +0200

    huhu

--- fdsmNew/src/execute/RunAll.java
+++ fdsmNew/src/execute/RunAll.java
@@ -15,28 +15,40 @@ import algo.Zscore;
 public class RunAll {
 
 	public static void main(String[] args) {
+		
 		Cooccurrence.run();
 		System.out.println("Co-occurrence finished"+System.lineSeparator());
+		
 		Cosine.run();
 		System.out.println("Cosine finished"+System.lineSeparator());
+		
 		Covariance.run();
 		System.out.println("Covariance finished"+System.lineSeparator());
+		
 		HypergeometricCoefficient.run();
 		System.out.println("Hypergeometric Coefficient finished"+System.lineSeparator());
+		
 		Jaccard.run();
 		System.out.println("Jaccard finished"+System.lineSeparator());
+		
 		LevFDSM.run();
 		System.out.println("LevFDSM finished"+System.lineSeparator());
+		
 		SLevFDSM.run();
 		System.out.println("SLevFDSM finished"+System.lineSeparator());
+		
 		PearsonCorrelation.run();
 		System.out.println("Pearson Correlation finished"+System.lineSeparator());
+		
 		PValue.run();
 		System.out.println("PValue finished"+System.lineSeparator());
+		
 		Zscore.run();
 		System.out.println("Zscore finished"+System.lineSeparator());
+		
 		PresortedZScore.run();
 		System.out.println("PresortedZScore finished"+System.lineSeparator());
+		
 	}
 
 }
